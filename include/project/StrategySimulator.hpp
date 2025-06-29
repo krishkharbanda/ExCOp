d9bd9137bb4bb83bb8d75acc42f34cb56be03cf1
Krish Kharbanda <kharbandakrish23@gmail.com>
2025-06-28 22:24:16 -0400

    simulate execution loop for backtesting strategies

--- include/project/StrategySimulator.hpp
+++ include/project/StrategySimulator.hpp
@@ -5,4 +5,12 @@
 #ifndef STRATEGYSIMULATOR_HPP
 #define STRATEGYSIMULATOR_HPP
 
+#include "OrderBook.hpp"
+#include "TraderAgent.hpp"
+
+class StrategySimulator {
+public:
+    void simulateTWAP(int qty, int intervals);
+};
+
 #endif //STRATEGYSIMULATOR_HPP
